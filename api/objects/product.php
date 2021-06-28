c5550a0d366572cee188990ec2b2d68cc2838676
Tipu Sultan Eiko <tipusultan50@gmail.com>
2021-06-28 16:14:07 +0600

    pagination

--- api/objects/product.php
+++ api/objects/product.php
@@ -218,4 +218,45 @@ class Product
 
         return $stmt;
     }
+
+    // read products with pagination
+    public function readPaging($from_record_num, $records_per_page){
+
+        // select query
+        $query = "SELECT
+                c.name as category_name, p.id, p.name, p.description, p.price, p.category_id, p.created
+            FROM
+                " . $this->table_name . " p
+                LEFT JOIN
+                    categories c
+                        ON p.category_id = c.id
+            ORDER BY p.created DESC
+            LIMIT ?, ?";
+
+        // prepare query statement
+        $stmt = $this->conn->prepare( $query );
+
+        // bind variable values
+        $stmt->bindParam(1, $from_record_num, PDO::PARAM_INT);
+        $stmt->bindParam(2, $records_per_page, PDO::PARAM_INT);
+
+        // execute query
+        $stmt->execute();
+
+        // return values from database
+        return $stmt;
+    }
+
+
+    // used for paging products
+    public function count(){
+        $query = "SELECT COUNT(*) as total_rows FROM " . $this->table_name . "";
+
+        $stmt = $this->conn->prepare( $query );
+        $stmt->execute();
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        return $row['total_rows'];
+    }
+
 }
