f4e29e0d6dceb37c201081922ad4f1e2c2222f05
Tipu Sultan Eiko <tipusultan50@gmail.com>
2021-06-28 11:33:00 +0600

    db connection and env

---
+++ api/config/database.php
@@ -0,0 +1,36 @@
+
+<?php
+include_once 'DotEnv.php';
+
+class Database{
+protected $host;
+protected $db_name;
+protected $username;
+protected $password;
+protected $conn;
+
+function __construct()
+{
+    (new DotEnv(__DIR__ . '/../.env'))->load();
+
+}
+
+
+public function connect(): ?PDO
+{
+    $this->host = getenv('DB_HOST');
+    $this->db_name = getenv('DB_DATABASE');
+    $this->username= getenv('DB_USERNAME');
+    $this->password= getenv('DB_PASSWORD');
+    $this->conn = null;
+
+    try {
+        $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password);
+        $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch(PDOException $e) {
+        echo 'Connection Error: ' . $e->getMessage();
+    }
+
+    return $this->conn;
+    }
+}
